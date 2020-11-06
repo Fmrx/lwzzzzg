27fed0b2ab98be5a23fac7ffe04757e309fc380f
qiankong <qiankong@micous.com>
2020-11-06 11:23:06 +0800

    add 代码自动生成

---
+++ lwzzzzg-core/src/main/java/com/lwzzzzg/service/impl/UmsMemberServiceImpl.java
@@ -0,0 +1,20 @@
+package com.lwzzzzg.service.impl;
+
+import com.lwzzzzg.entity.UmsMember;
+import com.lwzzzzg.mapper.UmsMemberMapper;
+import com.lwzzzzg.service.UmsMemberService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author fmrx
+ * @since 2020-11-05
+ */
+@Service
+public class UmsMemberServiceImpl extends ServiceImpl<UmsMemberMapper, UmsMember> implements UmsMemberService {
+
+}
